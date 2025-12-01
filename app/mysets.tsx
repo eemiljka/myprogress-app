02d9dcf78fcbdf889311539c58561423042a41ef
eemiljka <eemiljka@metropolia.fi>
2025-12-01 21:19:30 +0200

    viewing all sets on 'sets' page

--- app/mysets.tsx
+++ app/mysets.tsx
@@ -1,22 +1,48 @@
+import ParallaxScrollView from "@/components/parallax-scroll-view";
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
-import React from "react";
-import { Image, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { FlatList, Image, StyleSheet } from "react-native";
+import { API_URL } from "../config";
+
+interface Set {
+  id: number;
+  name: string;
+  rep_range?: string;
+}
 
 const MySets = () => {
+  const [sets, setSets] = useState<Set[]>([]);
+
+  useEffect(() => {
+    fetch(`${API_URL}/mysets`)
+      .then((res) => res.json())
+      .then((data) => setSets(data))
+      .catch((err) => console.error(err));
+  }, []);
   return (
-    <ThemedView>
-      <ThemedText style={styles.text}>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis
-        perspiciatis repellat nobis assumenda quidem autem provident corrupti
-        repellendus voluptates possimus aut, eligendi incidunt rerum vitae optio
-        corporis iste veniam recusandae.
-      </ThemedText>
-      <Image
-        style={styles.image}
-        source={require("@/assets/images/chart-placeholder.png")}
-      ></Image>
-    </ThemedView>
+    <ParallaxScrollView
+      headerImage={<ThemedText style={styles.normTitle}>Sets</ThemedText>}
+    >
+      <ThemedView style={styles.card}>
+        <FlatList
+          data={sets}
+          keyExtractor={(item, index) =>
+            item?.id ? item.id.toString() : index.toString()
+          }
+          renderItem={({ item }) => (
+            <ThemedText style={styles.cardTitle}>
+              {item.name} {item.rep_range}
+              {" reps"}
+            </ThemedText>
+          )}
+        />
+        <Image
+          style={styles.image}
+          source={require("@/assets/images/chart-placeholder.png")}
+        ></Image>
+      </ThemedView>
+    </ParallaxScrollView>
   );
 };
 
@@ -29,4 +55,28 @@ const styles = StyleSheet.create({
     resizeMode: "contain",
   },
   text: { padding: 16 },
+  cardTitle: {
+    marginBottom: 12,
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  card: {
+    backgroundColor: "#fff",
+    borderRadius: 16,
+    padding: 16,
+    marginBottom: 16,
+    // Shadow for iOS
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 5,
+    shadowOpacity: 0.15,
+    // Elevation for android
+    elevation: 4,
+  },
+  normTitle: {
+    padding: 16,
+    fontWeight: "bold",
+    fontSize: 24,
+    textAlign: "center",
+  },
 });
